0fd8dd81a61776cb50bed0a27d2ab7a258102df3
charlie763 <cwisoff@gmail.com>
2020-08-11 15:09:07 -0600

    write user model validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe Note, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model do
+  let(:valid_user_attributes) {
+      username: "RspecUser",
+      password: "password",
+      email: "test@test.com"
+  }
+
+  it "creates a user when a user with valid attributes is saved" do
+    valid_user = User.new(valid_user_attributes)
+    valid_user.save
+    expect(User.last.username).to eq(valid_user_attributes[:username])
+  end
+
+  it "doesn't create a user when the password is ill-formatted" do
+    invalid_user_attributes = valid_user_attributes
+    invalid_user_attributes[:email] = ".!Jonny@test@doubletest.com"
+    invalid_user = User.create(invalid_user_attributes)
+    expect(invalid_user).not_to be_valid
+  end
 end
