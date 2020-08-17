1f200f75b5bc7f9b2c1a9fb6cfa94467ba6eb37e
charlie763 <cwisoff@gmail.com>
2020-08-17 08:37:33 -0600

    write new action for external resources controller

--- app/controllers/external_resources_controller.rb
+++ app/controllers/external_resources_controller.rb
@@ -1,4 +1,10 @@
 class ExternalResourcesController < ApplicationController
   def new
+    @external_resource = ExternalResource.new()
+    @external_resource.language = Language.find_by(id: params[:language_id])
+  end
+
+  def create
+
   end
 end
