e588227c54e9188559de174de8bb41fd1dbbc3d5
charlie763 <cwisoff@gmail.com>
2020-08-18 16:39:56 -0600

    move user filter radio buttons to partial for notes search

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,6 +2,6 @@ class TopicsController < ApplicationController
   def show
     @language = Language.find_by(id: params[:language_id])
     @topic = Topic.find_by(id: params[:id])
-    @notes = Note.search(language: @language.name, topic: @topic.name)
+    @notes = Note.search({language: @language.name, topic: @topic.name}, current_user)
   end
 end
