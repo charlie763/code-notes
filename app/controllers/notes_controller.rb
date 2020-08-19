f76a501fd07f0c9776e3d2e82c9474d2858d92ce
charlie763 <cwisoff@gmail.com>
2020-08-19 16:26:06 -0600

    refactor notes controller helper methods

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,6 @@
 class NotesController < ApplicationController
+  include NotesHelper
+  
   def new
     @note = Note.new
     build_note_form
@@ -65,25 +67,5 @@ class NotesController < ApplicationController
 
   def language_params
     params.require(:note).permit(language_attributes: [:name])[:language_attributes]
-  end
-
-  def build_note_form
-    @note.build_language unless @note.language
-    @note.topics.build if @note.topics.empty?
-    @note.code_snippets.build if @note.code_snippets.empty?
-    @language_names = Language.possible_names
-    @topics = Topic.all
-  end
-
-  def validate_language
-    @note.language.valid?
-  end
-
-  def find_note_by_id
-    Note.find_by(id: params[:id])
-  end
-
-  def display_search_placeholder(attribute)
-    ": #{params[:terms][attribute]}" if params[:terms][attribute].present? 
   end
 end
--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,2 +1,21 @@
 module NotesHelper
+  def build_note_form
+    @note.build_language unless @note.language
+    @note.topics.build if @note.topics.empty?
+    @note.code_snippets.build if @note.code_snippets.empty?
+    @language_names = Language.possible_names
+    @topics = Topic.all
+  end
+
+  def find_note_by_id
+    Note.find_by(id: params[:id])
+  end
+
+  def display_search_placeholder(attribute)
+    ": #{params[:terms][attribute]}" if params[:terms][attribute].present? 
+  end
+
+  def validate_language
+    @note.language.valid?
+  end
 end
