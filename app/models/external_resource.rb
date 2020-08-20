789ec461410821c3ef9267d02f6a2b90cbd09ba2
charlie763 <cwisoff@gmail.com>
2020-08-20 12:51:16 -0600

    add validations for external_resources

--- app/models/external_resource.rb
+++ app/models/external_resource.rb
@@ -3,7 +3,17 @@ class ExternalResource < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :notes
 
+  validates :name, presence: true
+  validate :url_or_description_present
+
   def display_description
     description ? description : "(no description)"
+  end
+
+  private
+  def url_or_description_present
+    if !url && !description
+      errors.add(:url_or_description, "either a url or description must be given")
+    end
   end
 end
