ea40784298964e369f3c767e55cc1534c9392991
charlie763 <cwisoff@gmail.com>
2020-08-21 14:43:07 -0600

    add topic#destroy action

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -5,4 +5,11 @@ class TopicsController < ApplicationController
     @user = params[:terms] ? params[:terms][:user] : nil
     @notes = Note.search({language: @language.name, topic: @topic.name, user: @user}, current_user)
   end
+
+  def destroy
+    @note = Note.find(params[:note_id])
+    @topic = Topic.find(params[:id])
+    @note.topics.destroy(@topic)
+    redirect_to edit_note_path(@note)
+  end
 end
